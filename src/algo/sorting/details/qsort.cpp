de5a300a3e0f121b290b4019cd490d64aa56985f
Anatoly Nikiforov <Anatoly.Nikiforov@infowatch.com>
2015-09-30 13:43:09 +0300

    + algo: Dummy for sorting algos;

---
+++ src/algo/sorting/details/qsort.cpp
@@ -0,0 +1,5 @@
+#include "algo/sorting/qsort.h"
+
+void qsort(void* a, void* b, size_t count, size_t size) {
+    return;
+}
---
+++ src/algo/sorting/qsort.h
@@ -0,0 +1,13 @@
+/*! \file
+ *  \brief   Реализации быстрой сортировки
+ *  \author  Анатолий Никифоров
+ *  \date    2014-11-11
+ */
+
+#include <cstddef>
+
+namespace algo {
+
+void qsort(void* a, void* b, size_t count, size_t size);
+
+} // namespaces
